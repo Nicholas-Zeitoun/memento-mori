6e90701f1c797d69486dd5a299bf00ff06232ea0
NicholasZeitoun <nzeit8003@gmail.com>
2020-03-02 22:44:26 +1100

    Set up testing files for system testing   Added home visit test   Gems added: capybara, webdrivers, launchy

---
+++ test/system/home_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class HomeTest < ApplicationSystemTestCase
+  # [...]
+  test "Visits the home page" do
+    visit "/"
+    assert_selector "h1", text: "Pages#home"
+  end
+end
