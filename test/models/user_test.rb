23c3d91788fcd90423390b641a330f69c17134ac
NicholasZeitoun <nzeit8003@gmail.com>
2020-03-02 23:10:39 +1100

    Set up the rails models testing files

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should have an email address" do
+    user = User.new(email: "jeffrey@123.com")
+    assert_equal "jeffrey@123.com", user.email
+  end
+
+  test "Should have an assigned username" do
+    user = User.new(user_name: "jeffrey123")
+    assert_equal "jeffrey123", user.user_name
+  end
+
+  test "Should have a location Latitude" do
+    user = User.new(latitude: "-37.8378816")
+    assert_equal "-37.8378816", user.latitude
+  end
+
+  test "Should have a location Longitude" do
+    user = User.new(logitude: "144.9736116")
+    assert_equal "144.9736116", user.longitude
+  end
 end
