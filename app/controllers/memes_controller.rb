1bccb3737cf0425d4e4cc2c34a6509cf0c3f0f4e
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-03-05 08:49:16 +1100

    top three memes ready

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -7,6 +7,7 @@ class MemesController < ApplicationController
     @memes = Meme.all
     @top_categories = top_categories
     @top_three_users = top_three_users
+    @top_three_memes = top_three_memes
   end
 
   def show; end
@@ -60,9 +61,9 @@ class MemesController < ApplicationController
     .limit(10)
   end
 
-  # Retrieving top 3 users based on their dank rank
+  # Retrieving top 3 users based on their dank_rank.total_score
   def top_three_users
-    # Get top 3 dank ranks
+    # Get top 3 dank ranks total scores
     top_danks = DankRank.order('total_score DESC').limit(3)
     # For each top dank_rank, retrieve user
     top_danks.map do |dank|
@@ -70,6 +71,14 @@ class MemesController < ApplicationController
     end
   end
 
+  # Retrieving top 3 memes based on their rarity.total_score
+  def top_three_memes
+    # Get top 3 rarity total scores
+    top_rarities = Rarity.order('total_score DESC').limit(3)
+    top_rarities.map do |rarity|
+      Meme.find(rarity.meme_id)
+    end
+  end
 end
 
 
