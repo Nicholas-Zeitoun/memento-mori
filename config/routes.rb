2fea241328ab9bb4c4c1a7aea365ffb017a881b5
Ellen Zhuang <ellenzhuang88@gmail.com>
2020-03-06 14:18:03 +1100

    Added collection index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'memes#index'
   get 'style-guide', to: 'pages#style_guide'
+  get 'dashboard-template', to: 'pages#dashboard_template'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # users can't edit categories, can't create categories without creating a meme
