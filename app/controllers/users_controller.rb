c2b2859acd86e3f1a090291d7223541f70385889
NicholasZeitoun <nzeit8003@gmail.com>
2020-03-08 02:46:35 +1100

    Preppared user index leaderboard layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,21 @@ class UsersController < ApplicationController
 
   def index
     @users = users_ordered_by_dank
+    @top_three_users = top_three_users
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  # Retrieving top 3 users based on their dank_rank.total_score
+  def top_three_users
+    # Get top 3 dank ranks total scores
+    top_danks = DankRank.order('total_score DESC').limit(10)
+    # For each top dank_rank, retrieve user
+    top_danks.map do |dank|
+      User.find(dank.user_id)
+    end
   end
 
   private
