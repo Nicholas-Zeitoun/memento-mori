94f9d6c2c13e799ccbda93198731c8758b07f7ad
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-03-09 12:10:53 +1100

    added follow functionality for collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -12,5 +12,19 @@ class Collection < ApplicationRecord
       collfoll.follower
     end
   end
+
+  def follow(user_id)
+    CollectionFollowing.create!(follower: User.find(user_id), collection: self)
+  end
+
+  def unfollow(user_id)
+    # user_to_unfollow = User.find(user_id)
+    CollectionFollowing.find_by(follower: User.find(user_id)).destroy
+  end
+
+  def is_followed?(user_id)
+    relationship = CollectionFollowing.find_by(collection: self, follower: User.find(user_id))
+    return true if relationship
+  end
 end
 
