d3e21c152a8e9d2869ec996de495d589c2c39886
Ellen Zhuang <ellenzhuang88@gmail.com>
2020-03-09 18:29:06 +1100

    Added preview for upload image on meme new page, improved the UX of choosing a category or creating a new category

---
+++ app/javascript/packs/showImageCategory.js
@@ -0,0 +1,9 @@
+
+const imageCategory = document.querySelector(".new-category-input");
+const newCategoryButton = document.querySelector(".add-category");
+
+newCategoryButton.addEventListener('click', () => {
+  imageCategory.classList.toggle("d-none");
+})
+
+<!-- .style.display = "none"; -->
