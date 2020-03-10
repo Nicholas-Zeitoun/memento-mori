92fe5a9d616bd62fc81d4a07be585ab8785d2d20
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-03-10 11:20:11 +1100

    username now permitted by devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # To ask and allow user to add a username when signing up
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :location])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :location ])
     # To allow user to add an avatar when updating profile
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar_url])
   end
