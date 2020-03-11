bf3796ffa9a9c6c194509ed3c97cdd939ac1f3a0
Juliette Ferrer <juliette.ferrerpro@gmail.com>
2020-03-11 16:24:16 +1100

    second chart ready

--- app/javascript/packs/triangleChart.js
+++ app/javascript/packs/triangleChart.js
@@ -10,24 +10,31 @@ const scoreEngagement = Number.parseInt(chartDiv.dataset["engagement"], 10);
 
 // Create chart instance
 let chart = am4core.create("js-triangle-chart", am4charts.PieChart);
-chart.innerRadius = am4core.percent(60);
+chart.innerRadius = am4core.percent(40);
+chart.paddingBottom = 20;
 
 // Create pie series
 let series = chart.series.push(new am4charts.PieSeries());
 series.dataFields.value = "points";
 series.dataFields.category = "score";
+series.slices.template.propertyFields.fill = "color";
+series.labels.template.disabled = true;
 
 // Add data
 chart.data = [{
   "score": "Creation",
   "points": scoreCreation,
+  "color": am4core.color("#87629A")
 },{
   "score": "Collection",
-  "points": scoreCollection
+  "points": scoreCollection,
+  "color": am4core.color("#60367d")
 },{
   "score": "Engagement",
   "points": scoreEngagement,
+  "color": am4core.color("#39095F")
 }];
 
 // Add a legend
 chart.legend = new am4charts.Legend();
+chart.legend.labels.template.fill = am4core.color("#39095F");
