c7d7db16ba8a766097734e87ca6c28c8badbe31e
NicholasZeitoun <nzeit8003@gmail.com>
2020-03-11 20:35:50 +1100

    implemented meme rarity badges

--- app/models/rarity.rb
+++ app/models/rarity.rb
@@ -8,4 +8,29 @@ class Rarity < ApplicationRecord
     self.total_score = self.likes + self.comments + self.collects
     self.save!
   end
+
+  def get_rarity
+    case self.total_score
+    when 1
+      'blank-rank.png'
+    when 2
+      'bronze-rank.png'
+    when 3
+      'silver-rank.png'
+    when 4
+      'gold-rank.png'
+    when 5
+      'sapphire-rank.png'
+    when 6
+      'ruby-rank.png'
+    when 7
+      'emerald-rank.png'
+    when 8
+      'diamond-rank.png'
+    when 9
+      'diamond-rank.png'
+    else
+      'dank-rank.png'
+    end
+  end
 end
